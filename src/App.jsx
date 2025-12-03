381b58e943ebab6e863e1e5097e08c7d24490ce8
mh-lamine <mlamins.ngom@gmail.com>
2025-12-03 18:57:31 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import PWABadge from "./PWABadge.jsx";
+import "./App.css";
+import { Button } from "./components/ui/button.jsx";
+
+function App() {
+  return (
+    <>
+      <Button className="bg-brand">dsfd</Button>
+      <PWABadge />
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,52 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.js",
+      registerType: "prompt",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "WeConnect",
+        short_name: "WeConnect",
+        description:
+          "Plateforme de réservation pour les professionnels de beauté et soin",
+        theme_color: "#743089",
+      },
+
+      injectManifest: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+  server: {
+    port: 1406,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
